d46c9cc91acc2b6aaa7f962480666d107cd97bb9
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-10-23 14:27:08 -0400

    done with bonus tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,28 @@
+import { isPalindrome } from "../utils";
+
 // Your tests here
+describe("isPalindrome", () => {
+  it("returns true if the given word is a palindrome", () => {
+    const word = "racecar";
+    const answer = isPalindrome(word);
+    expect(answer).toBe(true);
+  });
+  it("should be case insensitive", () => {
+    const word = "RaCecAr";
+    const answer = isPalindrome(word);
+    expect(answer).toBe(true);
+  });
+  it("should return false for an empty string", () => {
+    const word = "";
+    const answer = isPalindrome(word);
+    expect(answer).toBe(false);
+  });
+  it("throws an error for non alpha numeric characters", () => {
+    const word = "race%ca*r";
+    expect(() => isPalindrome(word)).toThrow();
+  });
+  it("should throw an error for non strings", () => {
+    const word = 23;
+    expect(() => isPalindrome(word)).toThrow();
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,21 @@
 // Your code here
+export function isPalindrome(word) {
+  if (word === "") return false;
+
+  if (!/^[A-Za-z]+$/.test(word) || typeof word !== "string") {
+    throw new Error("invalid word");
+  }
+
+  const firstLetter = word[0];
+  const lastLetter = word[word.length - 1];
+  //base case
+  if (word.length <= 2) {
+    return firstLetter.toLowerCase() === lastLetter.toLowerCase();
+  }
+
+  //recursion
+  if (firstLetter.toLowerCase() === lastLetter.toLowerCase()) {
+    return isPalindrome(word.slice(1, -1));
+  }
+  return false;
+}
